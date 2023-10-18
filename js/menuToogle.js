f496966a89f629cde3e5bfd53789aef2a5541db5
Renan Caldas <renan@renancaldas.dev.br>
2023-10-18 09:30:20 -0300

    feat: header

--- js/menuToogle.js
+++ js/menuToogle.js
@@ -2,12 +2,16 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  const closeIcon = document.querySelector(".close-icon");
+function removeIconClose() {
+  const closeIcon = document.querySelector(".close-icon img");
   closeIcon.style.display = menu.classList.contains("active")
     ? "block"
     : "none";
+}
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  removeIconClose();
 });
 
 // JavaScript para fechar o menu ao clicar em um link
@@ -16,5 +20,6 @@ const menuLinks = document.querySelectorAll(".menu a");
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     menu.classList.remove("active");
+    removeIconClose();
   });
 });
